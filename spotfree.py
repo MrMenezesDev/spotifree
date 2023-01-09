84245f60c0082a16a7ed1f6efda4a28367a7f3eb
Erick Menezes <erick@mrmenezes.dev>
2023-01-09 14:37:45 -0300

    feat: update libs

--- spotfree.py
+++ spotfree.py
@@ -1,19 +1,19 @@
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtWidgets, QtGui
 import win32gui
 import pyautogui
 
 
-class Window(QtGui.QDialog):
+class Window(QtWidgets.QDialog):
     def __init__(self):
         super(Window, self).__init__()
-        self.messageGroupBox = QtGui.QGroupBox("")
+        self.messageGroupBox = QtWidgets.QGroupBox("")
         self.msg_wait = "Após clicar em iniciar vá para a tela do Spotify e aguarde 5 segundos"
         self.adv = False
         self.hwnd = None
         self.createMessageGroupBox()
         self.createActions()
         self.createTrayIcon()
-        mainLayout = QtGui.QVBoxLayout()
+        mainLayout = QtWidgets.QVBoxLayout()
         mainLayout.addWidget(self.messageGroupBox)
         self.setLayout(mainLayout)
 
@@ -65,54 +65,47 @@ class Window(QtGui.QDialog):
         self.hide()
 
     def createMessageGroupBox(self):
-        self.typeLabel = QtGui.QLabel(self.msg_wait)
-        self.iniciar_button = QtGui.QPushButton("Iniciar")
-        self.ok_button = QtGui.QPushButton("Ok")
+        self.typeLabel = QtWidgets.QLabel(self.msg_wait)
+        self.iniciar_button = QtWidgets.QPushButton("Iniciar")
+        self.ok_button = QtWidgets.QPushButton("Ok")
         self.ok_button.hide()
         self.iniciar_button.setDefault(True)
-        messageLayout = QtGui.QGridLayout()
+        messageLayout = QtWidgets.QGridLayout()
         messageLayout.addWidget(self.typeLabel, 0, 0)
         messageLayout.addWidget(self.iniciar_button, 1, 4)
         messageLayout.addWidget(self.ok_button, 1, 3)
         self.messageGroupBox.setLayout(messageLayout)
 
     def closeEvent(self, event):
-        if self.trayIcon.isVisible():
-            QtGui.QMessageBox.information(self, "SpotiFree",
-                                          "O programa continuará executando. Para sair "
-                                          "clique com botão direito no ícone no canto da tela"
-                                          " e em seguida clique em <b>Sair</b>.")
-            self.hide()
+        reply = QtWidgets.QMessageBox.question(self, 'Confirmação de fechamento', 'Tem certeza de que deseja fechar a aplicação?', QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No, QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            QtWidgets.QApplication.quit()
+        else:
             event.ignore()
 
+
     def createActions(self):
-        self.minimizeAction = QtGui.QAction("M&inimizar", self,
-                                            triggered=self.hide)
-
-        self.restoreAction = QtGui.QAction("&Restaurar", self,
-                                           triggered=self.showNormal)
-
-        self.quitAction = QtGui.QAction("&Sair", self,
-                                        triggered=QtGui.qApp.quit)
+        self.minimizeAction = QtWidgets.QAction("Minimizar", self, triggered=self.hide)
+        self.restoreAction = QtWidgets.QAction("Restaurar", self, triggered=self.showNormal)
+        self.quitAction = QtWidgets.QAction("Sair", self, triggered=self.close)
 
     def createTrayIcon(self):
-        self.trayIconMenu = QtGui.QMenu(self)
+        self.trayIconMenu = QtWidgets.QMenu(self)
         self.trayIconMenu.addAction(self.minimizeAction)
         self.trayIconMenu.addAction(self.restoreAction)
         self.trayIconMenu.addSeparator()
         self.trayIconMenu.addAction(self.quitAction)
-        self.trayIcon = QtGui.QSystemTrayIcon(self)
+
+        self.trayIcon = QtWidgets.QSystemTrayIcon(self)
         self.trayIcon.setContextMenu(self.trayIconMenu)
 
 
 if __name__ == '__main__':
-
     import sys
 
-    app = QtGui.QApplication(sys.argv)
-
-    QtGui.QApplication.setQuitOnLastWindowClosed(False)
+    app = QtWidgets.QApplication(sys.argv)
+    QtWidgets.QApplication.setQuitOnLastWindowClosed(False)
 
     window = Window()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
